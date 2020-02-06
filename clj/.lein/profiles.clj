dc5f21ca8ae8571dc0c7fb58db2149706ca33499
Lucas Lollobrigida <lucaslollobrigida@gmail.com>
2020-02-05 22:49:05 -0300

    fixes lein profile config been override

--- clj/.lein/profiles.clj
+++ clj/.lein/profiles.clj
@@ -1,8 +1,3 @@
-{:repl {:repl-options
-        {:init (clojure.core.server/start-server {:accept 'clojure.core.server/io-prepl
-                                                  :address "localhost"
-                                                  :port 5998
-                                                  :name "lein"})}}}
 {:user {:dependencies [[compliment "0.3.9"]
                        [slamhound "1.5.5"]
                        [pjstadig/humane-test-output "0.10.0"]]
@@ -12,6 +7,3 @@
                   [com.jakemccrary/lein-test-refresh "0.24.1"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]}}
-        ; :test-refresh {:notify-command ["terminal-notifier" "-title" "Tests" "-message"]
-        ;                        :quiet true
-        ;                        :changes-only true}}}
