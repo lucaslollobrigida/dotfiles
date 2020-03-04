32cff35a31ab37ae83b82b3358c07abd34e2256e
Lucas Lollobrigida <lucas.lollobrigida@nubank.com.br>
2020-03-03 22:08:48 -0300

    Fix lein profile override

--- clj/.lein/profiles.clj
+++ clj/.lein/profiles.clj
@@ -1,9 +1,17 @@
-{:user {:dependencies [[compliment "0.3.9"]
-                       [slamhound "1.5.5"]
-                       [pjstadig/humane-test-output "0.10.0"]]
-        :plugins [[lein-pprint "1.2.0"]
-                  [lein-kibit "0.1.7"]
-                  [pjstadig/humane-test-output "0.10.0"]
-                  [com.jakemccrary/lein-test-refresh "0.24.1"]]
-        :injections [(require 'pjstadig.humane-test-output)
-                     (pjstadig.humane-test-output/activate!)]}}
+{:user
+ {:min-lein-version    "2.9.0"
+  :repositories        [["central"  {:url "https://repo1.maven.org/maven2/" :snapshots false}]
+                        ["clojars"  {:url "https://clojars.org/repo/"}]
+                        ["nu-maven" {:url "s3p://nu-maven/releases/" :region "sa-east-1"}]]
+  :plugin-repositories [["nu-maven" {:url "s3p://nu-maven/releases/"}]]
+  :plugins             [[cider/cider-nrepl "0.23.0"]
+                        [lein-kibit "0.1.7"]
+                        [s3-wagon-private "1.3.3"]
+                        [lein-ancient "0.6.15"]]
+  :dependencies        [[cljdev "0.9.0"]]}
+
+ :repl {:plugins      [[cider/cider-nrepl "0.23.0"]
+                       [refactor-nrepl "2.5.0-SNAPSHOT"]]
+        :injections   [(require 'nu)]
+        :dependencies [[cljdev "0.9.0"]]
+        :repl-options {:timeout 120000}}}
